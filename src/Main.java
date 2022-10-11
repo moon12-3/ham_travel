d9726c844f21c489486e9fd7e18ecc62afd5aa97
문채영 <s2123@e-mirim.hs.kr>
2022-10-11 13:35:31 +0900

    상대경로 오류

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,25 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class Main {
     public static void main(String args[]) {
-        new Frame_make();
+        Frame_make fms = new Frame_make();
     }
 }
 
-class Frame_make extends JFrame {
+class Frame_make extends JFrame implements KeyListener, Runnable{
     int width = 1200;
     int height = 800;
+
+    int x, y; // 캐릭터의 좌표 변수
+    boolean KeyUp = false; //키보드 입력 처리를 위한 변수
+    boolean KeyDown = false;
+    boolean KeyLeft = false;
+    boolean KeyRight = false;
+    Thread th;  // 스레드 생성
+
     Toolkit tk = Toolkit.getDefaultToolkit();   // 이미지 불러오는 툴킷
     Image player = tk.getImage("../img/player.png");
 
@@ -20,7 +29,6 @@ class Frame_make extends JFrame {
         setSize(width, height);
         init();
         start();
-//        paint();
         Dimension screen = tk.getScreenSize();
 //        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize(); // 현재 모니터 해상도 값
         int xpos = (int) (screen.getWidth() / 2 - width / 2);
@@ -28,20 +36,75 @@ class Frame_make extends JFrame {
         setLocation(xpos, ypos);
         setVisible(true);
         setResizable(false);
-
     }
 
     public void init() { // 컴포넌트 세팅
+        x = 100; //캐릭터의 최초 좌표.
+        y = 100;
     }
 
     public void start() { // 시작처리명령
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addKeyListener(this);    //키보드 이벤트 실행
+        th = new Thread(this);  // 스레드 생성
+        th.start();
     }
 
+    @Override
+    public void run() {
+        try{ // 예외옵션 설정으로 에러 방지
+            while(true){ // while 문으로 무한 루프 시키기
+                KeyProcess(); // 키보드 입력처리를 하여 x,y 갱신
+                repaint(); // 갱신된 x,y값으로 이미지 새로 그리기
+                Thread.sleep(20); // 20 milli sec 로 스레드 돌리기
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
     public void paint(@NotNull Graphics g) {
-
         g.clearRect(0, 0, width, height); // 0,0 에서 위에서 정한 해상도 크기만큼 화면을 지움
-        g.drawImage(player, 100, 100, this); // (100, 100)에 player 이미지를 그려넣음
+        g.drawImage(player, x, y, this); // (x, y)에 player 이미지를 그려넣음
 
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch(e.getKeyCode()) {
+            case KeyEvent.VK_UP :
+            case KeyEvent.VK_W: KeyUp = true; break;
+            case KeyEvent.VK_DOWN :
+            case KeyEvent.VK_S : KeyDown = true; break;
+            case KeyEvent.VK_LEFT :
+            case KeyEvent.VK_A : KeyLeft = true; break;
+            case KeyEvent.VK_RIGHT :
+            case KeyEvent.VK_D : KeyRight = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        switch(e.getKeyCode()) {
+            case KeyEvent.VK_UP :
+            case KeyEvent.VK_W: KeyUp = false; break;
+            case KeyEvent.VK_DOWN :
+            case KeyEvent.VK_S : KeyDown = false; break;
+            case KeyEvent.VK_LEFT :
+            case KeyEvent.VK_A : KeyLeft = false; break;
+            case KeyEvent.VK_RIGHT :
+            case KeyEvent.VK_D : KeyRight = false;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    public void KeyProcess(){
+        if(KeyUp == true) y -= 5;
+        if(KeyDown == true) y += 5;
+        if(KeyLeft == true) x -= 5;
+        if(KeyRight == true) x += 5;
     }
 }
